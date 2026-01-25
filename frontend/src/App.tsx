f0a67860e7d7e172d4d31da7a2c266cf36a61202
joumaa alomar <oa6090449@gmail.com>
2026-01-25 23:52:50 +0300

    navbar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,11 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage";
-
+import NavBar from "./components/Navbar";
 function App() {
   return (
     <>
       <BrowserRouter>
+        <NavBar />
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="*" element={<div>404 Not Found</div>} />
